7aa87f1c4ef007b70ca22f7e9d499a948f392c71
jespermor3 <jespermor3@alum.us.es>
2024-03-15 12:17:42 +0100

    cambio

--- Proyecto/src/main/java/es/us/lsi/dad/RestServidor.java
+++ Proyecto/src/main/java/es/us/lsi/dad/RestServidor.java
@@ -1,4 +1,4 @@
-package dad;
+package es.us.lsi.dad;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -18,17 +18,18 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 
-public class RestSensor extends AbstractVerticle {
+public class RestServidor extends AbstractVerticle {
 	
 	private Map<Integer, Sensor> sensores=new HashMap<Integer, Sensor>();
 	private List<Sensor> lsen=new ArrayList<>();
 	private Gson gson;
 	
+	
 	public void start(Promise<Void> startFuture) {
 		createSomeData(25);
 		gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
 		Router router = Router.router(vertx);
-		vertx.createHttpServer().requestHandler(router::handle).listen(8084, result -> {
+		vertx.createHttpServer().requestHandler(router::handle).listen(8080, result -> {
 			if (result.succeeded()) {
 				startFuture.complete();
 			} else {
@@ -37,7 +38,7 @@ public class RestSensor extends AbstractVerticle {
 		});
 		
 		router.route("/api/sensores*").handler(BodyHandler.create());
-		router.get("/api/sensores").handler(this::getAllWithParams);
+		//router.get("/api/sensores").handler(this::getAllWithParams);
 		router.get("/api/sensores").handler(this::getAll);
 		router.get("/api/sensores/:id").handler(this::getOne);
 		router.post("/api/sensores").handler(this::addOne);
@@ -55,13 +56,13 @@ public class RestSensor extends AbstractVerticle {
 		
 	}
 	
-
-	private void getAllWithParams(RoutingContext routingContext) {
+	@SuppressWarnings("unused")
+	private void getAllWithParams(RoutingContext routingcontext1) {
 		lsen=sensores.entrySet().stream().map(x->x.getValue()).toList();
-		final String id = routingContext.queryParams().contains("id") ? routingContext.queryParam("id").get(0) : null;
-		final String nombre = routingContext.queryParams().contains("nombre") ? routingContext.queryParam("nombre").get(0) : null;
+		final String id = routingcontext1.queryParams().contains("id") ? routingcontext1.queryParam("id").get(0) : null;
+		final String nombre = routingcontext1.queryParams().contains("nombre") ? routingcontext1.queryParam("nombre").get(0) : null;
 		
-		routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(200)
+		routingcontext1.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(200)
 				.end(gson.toJson(lsen.stream().filter(elem ->{
 					boolean res = true;
 					res = res && id != null ? elem.getId().equals(Integer.valueOf(id)) : true;
@@ -69,53 +70,53 @@ public class RestSensor extends AbstractVerticle {
 					return res;
 				}).toList()));
 	}
-
-	private void getAll(RoutingContext routingContext) {
+	
+	private void getAll(RoutingContext routingcontext1) {
 		lsen=sensores.entrySet().stream().map(x->x.getValue()).toList();
-		routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(200)
+		routingcontext1.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(200)
 		.end(gson.toJson(lsen));
 	}
 
-	private void addOne(RoutingContext routingContext) {
-		final Sensor sensor = gson.fromJson(routingContext.getBodyAsString(), Sensor.class);
+	private void addOne(RoutingContext routingcontext1) {
+		final Sensor sensor = gson.fromJson(routingcontext1.getBodyAsString(), Sensor.class);
 		sensores.put(sensor.getId(), sensor);
-		routingContext.response().setStatusCode(201).putHeader("content-type", "application/json; charset=utf-8")
+		routingcontext1.response().setStatusCode(201).putHeader("content-type", "application/json; charset=utf-8")
 				.end(gson.toJson(sensor));
 	}
 
-	private void deleteOne(RoutingContext routingContext) {
-		int id = Integer.parseInt(routingContext.request().getParam("userid"));
+	private void deleteOne(RoutingContext routingcontext1) {
+		int id = Integer.parseInt(routingcontext1.request().getParam("userid"));
 		if (sensores.containsKey(id)) {
 			Sensor sensor = sensores.get(id);
 			sensores.remove(id);
-			routingContext.response().setStatusCode(200).putHeader("content-type", "application/json; charset=utf-8")
+			routingcontext1.response().setStatusCode(200).putHeader("content-type", "application/json; charset=utf-8")
 					.end(gson.toJson(sensor));
 		} else {
-			routingContext.response().setStatusCode(204).putHeader("content-type", "application/json; charset=utf-8")
+			routingcontext1.response().setStatusCode(204).putHeader("content-type", "application/json; charset=utf-8")
 					.end();
 		}
 	}
 
-	private void putOne(RoutingContext routingContext) {
-		int id = Integer.parseInt(routingContext.request().getParam("id"));
+	private void putOne(RoutingContext routingcontext1) {
+		int id = Integer.parseInt(routingcontext1.request().getParam("id"));
 		Sensor ds = sensores.get(id);
-		final Sensor element = gson.fromJson(routingContext.getBodyAsString(), Sensor.class);
+		final Sensor element = gson.fromJson(routingcontext1.getBodyAsString(), Sensor.class);
 		ds.setNombre(element.getNombre());
 		ds.setFecha(element.getFecha());
 		ds.setValor(element.getValor());
 		sensores.put(ds.getId(), ds);
-		routingContext.response().setStatusCode(201).putHeader("content-type", "application/json; charset=utf-8")
+		routingcontext1.response().setStatusCode(201).putHeader("content-type", "application/json; charset=utf-8")
 				.end(gson.toJson(element));
 	}
 
-	private void getOne(RoutingContext routingContext) {
-		int id = Integer.parseInt(routingContext.request().getParam("userid"));
+	private void getOne(RoutingContext routingcontext1) {
+		int id = Integer.parseInt(routingcontext1.request().getParam("userid"));
 		if (sensores.containsKey(id)) {
 			Sensor ds = sensores.get(id);
-			routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(200)
+			routingcontext1.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(200)
 					.end(gson.toJson(ds));
 		} else {
-			routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(204)
+			routingcontext1.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(204)
 					.end();
 		}
 	}
---
+++ Proyecto/src/main/java/es/us/lsi/dad/Sensor.java
@@ -0,0 +1,90 @@
+package es.us.lsi.dad;
+
+import java.util.Objects;
+
+public class Sensor {
+	private Integer id;
+	private String nombre;
+	private Long fecha;
+	private Double valor;
+	
+	
+	public Sensor(Integer id, String nombre, Long fecha, Double valor) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.fecha = fecha;
+		this.valor = valor;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public Long getFecha() {
+		return fecha;
+	}
+
+
+	public void setFecha(Long fecha) {
+		this.fecha = fecha;
+	}
+
+
+	public Double getValor() {
+		return valor;
+	}
+
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(fecha, id, nombre, valor);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sensor other = (Sensor) obj;
+		return Objects.equals(fecha, other.fecha) && Objects.equals(id, other.id)
+				&& Objects.equals(nombre, other.nombre) && Objects.equals(valor, other.valor);
+	}
+
+
+	@Override
+	public String toString() {
+		return "Sensor [id=" + id + ", nombre=" + nombre + ", fecha=" + fecha + ", valor=" + valor + "]";
+	}
+	
+	
+	
+	
+
+}
